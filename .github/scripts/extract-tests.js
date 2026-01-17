4e35dece31d65bf64d3b573bb7ecb76a846998c8
kritikajain-03 <jkritika@concret.io>
2026-01-17 21:19:15 +0530

    add files

--- .github/scripts/extract-tests.js
+++ .github/scripts/extract-tests.js
@@ -1,16 +1,12 @@
 const fs = require('fs');
-
 const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
 const body = event.pull_request?.body || '';
 
 let testsToRun = 'all';
-
 const match = body.match(/Apex::\[(.*?)\]::Apex/);
-
 if (match && match[1]) {
   testsToRun = match[1].trim();
 }
 
 fs.writeFileSync('testsToRun.txt', testsToRun);
-
 console.log('Tests to run:', testsToRun);
